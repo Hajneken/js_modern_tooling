c1dc48e2228b38ddbb2a083dbe83469d681a32ba
Hynek Zemanec <zemanec@fg.cz>
2019-10-16 17:14:07 +0200

    newly created files, webpack, styles

---
+++ webpack.config.dev.js
@@ -0,0 +1,12 @@
+// dev specific config
+
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.config.base')
+
+module.exports = merge(baseConfig, {
+    mode:'development',
+    devServer:{
+        port: 9000
+    },
+    devtool: 'source-map'
+})
---
+++ webpack.config.prod.js
@@ -0,0 +1,9 @@
+// production config specific 
+
+const merge = require('webpack-merge');
+
+const baseConfig = require('./webpack.config.base')
+
+module.exports = merge(baseConfig, {
+    mode:'production'
+})
